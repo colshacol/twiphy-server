e3d8c3ed9e34923db639f40f00784c831371c940
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-07-22 17:15:24 -0500

    added type to argument

--- source/utils/createShortId.js
+++ source/utils/createShortId.js
@@ -6,7 +6,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export default function(length = 4) {
+export default function(length: number = 4) {
   let id = ''
   for(let i = 0; i < length; i++) {
     let rando = getRandomInt(0, chars.length-1)
