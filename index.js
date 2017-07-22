67cb98ebfab9329abf3ff57834d28bee33789ac1
Colcleasure, Colton <Colton.Colcleasure.ctr@sabre.com>
2017-07-22 17:25:02 -0500

    Idk what I did, but it works.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
+require('babel-register');
 
 global.log = (...messages) => {
   console.log('\n[twiphy-server] |\n\n\ \ ', ...messages);
 }
 
 require('regenerator-runtime/runtime');
-require('babel-register');
 require('./source');
--- source/utils/createShortId.js
+++ source/utils/createShortId.js
@@ -6,8 +6,8 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export default function(length: number = 4) {
-  let id = ''
+export default function dog(length: number = 4) {
+  let id = '';
   for(let i = 0; i < length; i++) {
     let rando = getRandomInt(0, chars.length-1)
     id += chars[rando]
